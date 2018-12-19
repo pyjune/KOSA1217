4c3f3bf3c01a4b81e5a4864844324909d94c04cd
pyjune <pyjune@naver.com>
2018-12-19 14:02:58 +0900

    Update 미로최단거리.c

--- day3/미로최단거리.c
+++ day3/미로최단거리.c
@@ -9,6 +9,9 @@ void find(int r, int c);
 int minV;
 void find2(int r, int c, int e);
 
+int q[10000];
+int visited[100][100];
+int find3(int r, int c);
 
 int main(void) 
 {
@@ -37,10 +40,49 @@ int main(void)
 		}
 		//find(sr, sc);
 		//printf("#%d %d\n", tc, cnt);
-		find2(sr, sc, 0);
-		if( minV == 0x7fffffff)
-			minV = 1;
-		printf("#%d %d\n", tc, minV-1);
+//		find2(sr, sc, 0);
+//		if( minV == 0x7fffffff)
+//			minV = 1;
+//		printf("#%d %d\n", tc, minV-1);
+		printf("#%d %d\n", tc, find3(sr, sc));
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < N; j++)
+			{
+				visited[i][j] = 0;
+			}
+		}
+		
+	}
+	return 0;
+}
+
+int find3(int r, int c)
+{
+	int dr[] = {0, 1, 0, -1};
+	int dc[] = {1, 0, -1, 0};
+	int front = -1;
+	int rear = -1;
+	q[++rear] = r;
+	q[++rear] = c;
+	visited[r][c] = 1;
+	while(front != rear)
+	{
+		r = q[++front];
+		c = q[++front];
+		if(maze[r][c] == 3)
+			return (visited[r][c] - 2);
+		for(int i = 0; i < 4; i++)
+		{
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if(maze[nr][nc] != 1 && visited[nr][nc] == 0)
+			{
+				q[++rear] = nr;
+				q[++rear] = nc;
+				visited[nr][nc] = visited[r][c] + 1;	
+			}
+		}
 	}
 	return 0;
 }
